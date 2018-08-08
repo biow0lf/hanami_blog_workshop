63616720cc57eac5d33c3811f4e1f58efd96df8a
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-08 19:44:48 +0300

    Add spec

--- spec/web/controllers/posts/index_spec.rb
+++ spec/web/controllers/posts/index_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe Web::Controllers::Posts::Index, type: :action do
-  let(:action) { described_class.new }
+  let(:post) { Post.new(id: 1, title: 'Hello World', body: 'World') }
+
+  let(:repository) { double('repository', all: [post]) }
+
+  let(:action) { described_class.new(repository: repository) }
+
   let(:params) { Hash[] }
 
   it 'is successful' do
     response = action.call(params)
-    expect(response[0]).to eq 200
+
+    expect(response[0]).to eq(200)
+
+    expect(action.posts).to eq([post])
+
+    expect(action.exposures[:posts]).to eq([post])
   end
 end
