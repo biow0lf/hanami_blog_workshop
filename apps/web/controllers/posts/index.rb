1cd195539a198ada88d980425be551e44d326172
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-08 19:00:39 +0300

    Working posts#index

--- apps/web/controllers/posts/index.rb
+++ apps/web/controllers/posts/index.rb
@@ -2,7 +2,12 @@ module Web::Controllers::Posts
   class Index
     include Web::Action
 
+    expose :posts
+
     def call(params)
+      @repository = PostRepository.new
+
+      @posts = @repository.all
     end
   end
 end
