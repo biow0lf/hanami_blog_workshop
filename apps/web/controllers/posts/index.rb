08422a28f7856309425cfdbd45ba308880dac6f0
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-08 19:20:47 +0300

    DI PostRepository

--- apps/web/controllers/posts/index.rb
+++ apps/web/controllers/posts/index.rb
@@ -4,9 +4,11 @@ module Web::Controllers::Posts
 
     expose :posts
 
-    def call(params)
-      @repository = PostRepository.new
+    def initialize(repository: PostRepository.new)
+      @repository = repository
+    end
 
+    def call(params)
       @posts = @repository.all
     end
   end
