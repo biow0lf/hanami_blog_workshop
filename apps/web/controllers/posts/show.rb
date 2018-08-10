fdb116d5ffc4ea28fc99c857b10680d5930f3062
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-10 17:52:27 +0300

    Test view

--- apps/web/controllers/posts/show.rb
+++ apps/web/controllers/posts/show.rb
@@ -2,7 +2,10 @@ module Web::Controllers::Posts
   class Show
     include Web::Action
 
+    expose :post
+
     def call(params)
+      @post = PostRepository.new.find(params[:id])
     end
   end
 end
--- spec/web/views/posts/show_spec.rb
+++ spec/web/views/posts/show_spec.rb
@@ -1,10 +1,21 @@
 RSpec.describe Web::Views::Posts::Show, type: :view do
-  let(:exposures) { Hash[format: :html] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/posts/show.html.erb') }
-  let(:view)      { described_class.new(template, exposures) }
-  let(:rendered)  { view.render }
+  let(:template) { Hanami::View::Template.new('apps/web/templates/posts/show.html.erb') }
+
+  let(:exposures) { Hash[post: double('post', title: 'Hello', body: 'World'), params: { id: 1 }, format: :html] }
+
+  let(:view) { described_class.new(template, exposures) }
+
+  let(:rendered) { view.render }
 
   it 'exposes #format' do
-    expect(view.format).to eq exposures.fetch(:format)
+    expect(view.format).to eq(:html)
+  end
+
+  it 'show post title' do
+    expect(rendered).to include('<h2>Hello</h2>')
+  end
+
+  it 'show post body' do
+    expect(rendered).to include('<p>World</p>')
   end
 end
