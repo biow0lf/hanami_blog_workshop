28665d237a192a9e8cea657aade4a6c14348d784
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-11 17:46:35 +0300

    Destroy posts

--- apps/web/controllers/posts/destroy.rb
+++ apps/web/controllers/posts/destroy.rb
@@ -2,7 +2,14 @@ module Web::Controllers::Posts
   class Destroy
     include Web::Action
 
+    def initialize(repository: PostRepository.new)
+      @repository = repository
+    end
+
     def call(params)
+      @repository.delete(params[:id])
+
+      redirect_to routes.path(:posts)
     end
   end
 end
