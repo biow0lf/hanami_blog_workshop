cb80089a17087bf1bf581f067f75f788e3b56f40
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-11 14:20:11 +0300

    Create new post form

--- apps/web/controllers/posts/create.rb
+++ apps/web/controllers/posts/create.rb
@@ -2,7 +2,14 @@ module Web::Controllers::Posts
   class Create
     include Web::Action
 
+    def initialize(repository: PostRepository.new)
+      @repository = repository
+    end
+
     def call(params)
+      post = @repository.create(params[:post])
+
+      redirect_to routes.path(:post, id: post.id)
     end
   end
 end
