a4ee2d4177c2f068eae5c18797bcf275aef9cf9d
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-11 16:57:49 +0300

    Update post

--- apps/web/controllers/posts/update.rb
+++ apps/web/controllers/posts/update.rb
@@ -2,7 +2,23 @@ module Web::Controllers::Posts
   class Update
     include Web::Action
 
+    params do
+      required(:post).schema do
+        required(:title).filled(:str?)
+        required(:body).filled(:str?)
+      end
+    end
+
+    def initialize(repository: PostRepository.new)
+      @repository = repository
+    end
+
     def call(params)
+      if params.valid?
+        post = @repository.update(params[:id], params[:post])
+
+        redirect_to routes.path(:post, id: post.id)
+      end
     end
   end
 end
