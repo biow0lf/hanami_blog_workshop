66c97b5fd473d47b2c916a28047bc483ec86de56
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-13 16:50:45 +0300

    Cleanup

--- spec/web/controllers/posts/create_spec.rb
+++ spec/web/controllers/posts/create_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe Web::Controllers::Posts::Create, type: :action do
-  let(:action) { described_class.new }
-  let(:params) { Hash[] }
-
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
-  end
 end
