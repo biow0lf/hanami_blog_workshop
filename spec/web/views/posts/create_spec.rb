029831541291972ab05d11cfc12d77b8c6317679
Igor Zubkov <igor.zubkov@gmail.com>
2018-08-13 16:52:05 +0300

    Cleanup

--- spec/web/views/posts/create_spec.rb
+++ spec/web/views/posts/create_spec.rb
@@ -1,10 +1,2 @@
 RSpec.describe Web::Views::Posts::Create, type: :view do
-  let(:exposures) { Hash[format: :html] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/posts/create.html.erb') }
-  let(:view)      { described_class.new(template, exposures) }
-  let(:rendered)  { view.render }
-
-  it 'exposes #format' do
-    expect(view.format).to eq exposures.fetch(:format)
-  end
 end
--- spec/web/views/posts/update_spec.rb
+++ spec/web/views/posts/update_spec.rb
@@ -1,10 +1,2 @@
 RSpec.describe Web::Views::Posts::Update, type: :view do
-  let(:exposures) { Hash[format: :html] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/posts/update.html.erb') }
-  let(:view)      { described_class.new(template, exposures) }
-  let(:rendered)  { view.render }
-
-  it 'exposes #format' do
-    expect(view.format).to eq exposures.fetch(:format)
-  end
 end
